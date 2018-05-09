43abcc81e674bf33fc4480d997ef88a3350d8ac3
illusiongame01 <39041011+illusiongame01@users.noreply.github.com>
2018-05-09 15:19:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,8 @@ app.post('/', function (req, res) {
          let balance = '2';
           agent.add("ตรวจสอบยอดเงินคงเหลือ")
            var request = unirest("GET", "https://110.49.202.87:8443/GoogleAssistant/GetCurrentBalacnce/66932780014").strictSSL(false);     
-             agent.add("สวัสดี")            
+             agent.add("สวัสดี")     
+             sleep(2000);
               request.end(function(res) {
                 if(res.error) {
                     console.log(res.error)
