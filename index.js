5bc3a74df0a2367fe152b6f48c20c22b4370fb36
illusiongame01 <39041011+illusiongame01@users.noreply.github.com>
2018-05-16 17:23:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ app.post('/', (req, res) => {
          
             title: `Sim 2 Fly`,
             imageUrl: `https://store.ais.co.th/media/wysiwyg/product/product-description/Sim/SIM2Fly_LINEHome1040x1040_Compress.jpg`,
-            text: `Sim 2 Fly โรมมิ่ง ราคาประหยัด`,
+            text: `<a href="https://www.w3schools.com">Sim 2 Fly โรมมิ่ง ราคาประหยัด</a>`,
             buttonText: `ดูข้อมูลเพิ่มเติม`,
             buttonUrl: `http://www.ais.co.th/roaming/sim2fly/?gclid=CjwKCAjww6XXBRByEiwAM-ZUIFrTKb_iEnZqewsMkYG8kFvliueHR1sX3-cFfQPo_hvcGtiRbo_68RoC1SIQAvD_BwE&s_kwcid=AL!897!3!259718486577!e!!g!!sim2fly&ef_id=WnKrygAAAdEwtceS:20180502080316:s`,
           
