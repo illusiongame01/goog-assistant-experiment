fc1c66616f8b18ff992778372baa9b75c4410a19
illusiongame01 <39041011+illusiongame01@users.noreply.github.com>
2018-05-16 13:53:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -196,17 +196,15 @@ app.post('/', (req, res) => {
                    
                 }
             }
-        }))
-     
-     
-      conv.ask(new Card({
+        }))    
+        agent.add(conv)
+        agent.add(new Card({
             title: `Sim 2 Fly`,
             imageUrl: `https://store.ais.co.th/media/wysiwyg/product/product-description/Sim/SIM2Fly_LINEHome1040x1040_Compress.jpg`,
             text: `Sim 2 Fly โรมมิ่ง ราคาประหยัด`,
             buttonText: `ดูข้อมูลเพิ่มเติม`,
             buttonUrl: `http://www.ais.co.th/roaming/sim2fly/?gclid=CjwKCAjww6XXBRByEiwAM-ZUIFrTKb_iEnZqewsMkYG8kFvliueHR1sX3-cFfQPo_hvcGtiRbo_68RoC1SIQAvD_BwE&s_kwcid=AL!897!3!259718486577!e!!g!!sim2fly&ef_id=WnKrygAAAdEwtceS:20180502080316:s`,
         }))
-        agent.add(conv)
     }
 
     function onTopHandler(agent) {
