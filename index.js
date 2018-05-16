88a738a37055cf9fa093caf6449cce47c16f7798
illusiongame01 <39041011+illusiongame01@users.noreply.github.com>
2018-05-16 16:58:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -235,6 +235,7 @@ app.post('/', (req, res) => {
             buttonText: `ดูข้อมูลเพิ่มเติม`,
             buttonUrl: `http://www.ais.co.th/roaming/sim2fly/?gclid=CjwKCAjww6XXBRByEiwAM-ZUIFrTKb_iEnZqewsMkYG8kFvliueHR1sX3-cFfQPo_hvcGtiRbo_68RoC1SIQAvD_BwE&s_kwcid=AL!897!3!259718486577!e!!g!!sim2fly&ef_id=WnKrygAAAdEwtceS:20180502080316:s`, 
                     }
+             }
         }))
                     
          agent.add(conv)
