4cd28ff0d60e3509d143f01b6311967f6a2412f1
illusiongame01 <39041011+illusiongame01@users.noreply.github.com>
2018-05-18 08:58:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /*const { SimpleResponse, Carousel , Image , BasicCard , BrowseCarousel} = require('dialogflow-fulfillment/node_modules/actions-on-google/dist/service/actionssdk');*/
-const {  dialogflow,  BasicCard,  BrowseCarousel, BrowseCarouselItem , Button,  Carousel,  Image,  LinkOutSuggestion,  List,  MediaObject,  Suggestions,  SimpleResponse } = require('dialogflow-fulfillment/node_modules/actions-on-google/dist/service/actionssdkactionssdk/api/v2');
+const {  dialogflow,  BasicCard,  BrowseCarousel, BrowseCarouselItem , Button,  Carousel,  Image,  LinkOutSuggestion,  List,  MediaObject,  Suggestions,  SimpleResponse } = require('dialogflow-fulfillment/node_modules/actions-on-google/dist/service/actionssdkactionssdk');
+
 const processor = require('./processor')
 const express = require('express'),
     bodyParser = require('body-parser')
